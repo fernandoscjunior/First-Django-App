2b21ffc46e4baa75146db8355d6bfe2f53fb03e3
fernandoscjunior <juniorsud2006@gmail.com>
2021-11-05 16:23:02 -0300

    Adicionando Banco de Dados Postgresql na aplicação.

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    receitas = {
+        1:'Lasanha',
+        2:'Bolo',
+        3:"Sorvete"
+    }
+
+    dados = {
+        'nomes_das_receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
 
 def receita(request):
     return render(request, 'receita.html')
