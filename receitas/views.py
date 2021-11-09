3a254558f9996e0a1f8fde3260c2bd7acefeff1a
fernandoscjunior <juniorsud2006@gmail.com>
2021-11-09 17:02:39 -0300

    Adicionando banco de dados funcional e configurando admin

--- receitas/views.py
+++ receitas/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 from django.http import HttpResponse
+from .models import Receita
 
 def index(request):
-    receitas = {
-        1:'Lasanha',
-        2:'Bolo',
-        3:"Sorvete"
-    }
+    receitas = Receita.objects.all()
 
     dados = {
-        'nomes_das_receitas': receitas
+        'receitas': receitas
     }
 
     return render(request, 'index.html', dados)
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_exibir = {
+        'receita' : receita
+    }
+
+    return render(request, 'receita.html', receita_exibir)
