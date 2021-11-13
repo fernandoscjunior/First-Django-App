7520e2e17849c2c51fb5ab9c7bac5cf41f364f2e
fernandoscjunior <juniorsud2006@gmail.com>
2021-11-13 15:29:03 -0300

    configurando o admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita', )
+    list_filter = ('categoria', )
+    lista_per_page = 50
+
+admin.site.register(Receita, ListandoReceitas)
