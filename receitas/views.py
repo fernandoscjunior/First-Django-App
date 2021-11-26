a1c0794213e5de0af1ea6dee0f17619a9702f0f1
fernandoscjunior <juniorsud2006@gmail.com>
2021-11-26 19:52:49 -0300

    terminando o sistema de busca por receitas

--- receitas/views.py
+++ receitas/views.py
@@ -20,4 +20,16 @@ def receita(request, receita_id):
     return render(request, 'receita.html', receita_exibir)
 
 def buscar(request):
-    return render(request, 'buscar.html')
+    lista_receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if buscar:
+            lista_receitas = lista_receitas.filter(nome_receita__icontains = nome_a_buscar)
+
+    dados = {
+        'receitas' : lista_receitas
+
+    }
+
+    return render(request, 'buscar.html', dados)
