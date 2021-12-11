a48c9bdd3ae54fb43b3895956c717dca6ad5b426
fernandoscjunior <juniorsud2006@gmail.com>
2021-12-10 22:21:22 -0300

    Aprimorando a questão de login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+
+        if not nome.strip():
+            print('')
+            return redirect('cadastro')
+        if not email.strip():
+            print('')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('')
+        if User.objects.filter(email=email).exists():
+            print('')
+
+        user = User.objects.create_user(username=nome, email=email, password=senha, )
+
+        print(nome, email, senha, senha2)
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
     pass
 
 def dashboard(request):
-    pass
+    return render(request, 'usuarios/dashboard.html')
